474ef908619128cd09524ac195f976b9b942062a
Yura <mr.Shapovaloff3101@gmail.com>
2021-09-25 22:47:44 +0300

    3.2. Добавит моки reviews.

---
+++ project/src/mocks/reviews.js
@@ -0,0 +1,58 @@
+const AVATAR_URL = 'https://i.pravatar.cc/54';
+
+const reviews = [
+  {
+    comment:
+      'Our tours are unique. They are designed to bring a greater awareness of our heritage to the people of the island. We will go on a boat tour to the island, then we will take a kayak tour to the island. The kayak tour will be the most scenic one. You will see the main island and then you will see the islands surrounding it. The kayak tour will be a bit more of a cruise boat trip.',
+    date: '2021-04-08T14:13:56.569Z',
+    id: 1,
+    rating: 3,
+    user: {
+      avatarUrl: AVATAR_URL,
+      id: 1,
+      isPro: false,
+      name: 'Max',
+    },
+  },
+  {
+    comment:
+      'Were able to get a great overview of what the city had to offer and what made it such a great place to live. Were able to see the history of the city and where it had been for the past 10,000 years.We also took a tour of the beautiful gardens of the city and how they were filled with flora and fauna.',
+    date: '2021-03-08T14:15:56.569Z',
+    id: 2,
+    rating: 2,
+    user: {
+      avatarUrl: AVATAR_URL,
+      id: 2,
+      isPro: true,
+      name: 'Robert',
+    },
+  },
+  {
+    comment:
+      'All of my tours are completely guided by Jon. They range from a simple introduction to the area to an intimate tour of a place and the people that live there, along with their own personal stories. Jon can be incredibly helpful in helping you narrow down your choices and make the right decisions for your visit to the area.We are also fully self-sustaining and self-employed so we have plenty of time to explore the area and visit as many places as possible.',
+    date: '2021-02-08T14:16:56.569Z',
+    id: 3,
+    rating: 5,
+    user: {
+      avatarUrl: AVATAR_URL,
+      id: 3,
+      isPro: false,
+      name: 'Chris',
+    },
+  },
+  {
+    comment:
+      'There are a number of groups offering guided tours of the park. I would highly recommend taking a group tour, especially for people who have never been to the park before. You will have a more hands-on experience and you will be given a tour guide who will show you the most interesting spots of the park along with the most important places such as the National Park.',
+    date: '2021-01-08T14:20:56.569Z',
+    id: 4,
+    rating: 4,
+    user: {
+      avatarUrl: AVATAR_URL,
+      id: 4,
+      isPro: true,
+      name: 'Leo',
+    },
+  },
+];
+
+export default reviews;
