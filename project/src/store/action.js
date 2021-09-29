02d8bd228f245a2027193d166dadc417a50ac642
Yura <mr.Shapovaloff3101@gmail.com>
2021-09-30 00:42:40 +0300

    5.5. Добавит ActionCreator.

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -2,3 +2,14 @@ export const ActionType = {
   CHANGE_CITY: 'cities/changeCity',
   FILL_OFFERS_LIST: 'offers/fillOffersList',
 };
+
+export const ActionCreator = {
+  changeCity: (city) => ({
+    type: ActionType.CHANGE_CITY,
+    payload: city,
+  }),
+  fillOffersList: (city) => ({
+    type: ActionType.FILL_OFFERS_LIST,
+    payload: city,
+  }),
+};
