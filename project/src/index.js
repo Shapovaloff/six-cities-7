2acc256d248f8db562bda60c617d2d7600c77e46
Yura <mr.Shapovaloff3101@gmail.com>
2021-09-30 00:38:09 +0300

    5.3. Подключит redux к проекту.

--- project/src/index.js
+++ project/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
+import {createStore} from '@reduxjs/toolkit';
+import {reducer} from './store/reducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App
-      offers={offers}
-      reviews={reviews}
-    />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App
+        offers={offers}
+        reviews={reviews}
+      />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'));
