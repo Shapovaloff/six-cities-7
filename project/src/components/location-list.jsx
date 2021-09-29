86045ebd4fafc1bf394a5c90c3d944e94c17a12a
Yura <mr.Shapovaloff3101@gmail.com>
2021-09-30 01:29:34 +0300

    5.9. Добавит функции переключения городов в LocationList.

--- project/src/components/location-list.jsx
+++ project/src/components/location-list.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {ActionCreator} from '../store/action';
 
 function LocationList(props) {
-  const {location} = props;
+  const {location, city, changeCity, fillOffersList} = props;
 
   return (
     <ul className="locations__list tabs__list">
@@ -11,7 +13,15 @@ function LocationList(props) {
 
         return (
           <li key={keyValue} className="locations__item">
-            <a className={`${id === 3 && 'tabs__item--active'} locations__item-link tabs__item`} href="#!">
+            <a
+              className={`${item === city && 'tabs__item--active'} locations__item-link tabs__item`}
+              href="#!"
+              onClick={(evt) => {
+                evt.preventDefault();
+                changeCity(item);
+                fillOffersList(item);
+              }}
+            >
               <span>{item}</span>
             </a>
           </li>
@@ -23,6 +33,19 @@ function LocationList(props) {
 
 LocationList.propTypes = {
   location: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  city: PropTypes.string.isRequired,
+  changeCity: PropTypes.func.isRequired,
+  fillOffersList: PropTypes.func.isRequired,
 };
 
-export default LocationList;
+const mapStateToProps = (state) => ({
+  city: state.city,
+});
+
+const mapDispatchToProps = {
+  changeCity: ActionCreator.changeCity,
+  fillOffersList: ActionCreator.fillOffersList,
+};
+
+export {LocationList};
+export default connect(mapStateToProps, mapDispatchToProps)(LocationList);
