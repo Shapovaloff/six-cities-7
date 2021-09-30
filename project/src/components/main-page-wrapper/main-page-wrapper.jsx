8606e8981264c97b077748217a9aa177b43657a6
Yura <mr.Shapovaloff3101@gmail.com>
2021-09-30 12:35:29 +0300

    5.12. Рефа3ºторинг MainPage.

---
+++ project/src/components/main-page-wrapper/main-page-wrapper.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SortForm from '../sort-form/sort-form';
+import PlaceCard from '../place-card/place-card';
+import {ClassesCardType} from '../../const';
+import Map from '../map/map';
+import offerProp from '../app/offer.prop';
+
+function MainPageWrapper(props) {
+  const {offers, city} = props;
+
+  return (
+    <div className="cities__places-container container">
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">{offers.length} places to stay in {city}</b>
+        <SortForm />
+        <div className="cities__places-list places__list tabs__content">
+          {offers.map((offer, id) => {
+            const keyValue = `place-card-${id}`;
+            return <PlaceCard key={keyValue} offer={offer} cardType={ClassesCardType.MAIN} />;
+          })}
+        </div>
+      </section>
+      <div className="cities__right-section">
+        <section className="cities__map map">
+          <Map city={offers[0].city} offers={offers} />
+        </section>
+      </div>
+    </div>
+  );
+}
+
+MainPageWrapper.propTypes = {
+  offers: PropTypes.arrayOf(offerProp).isRequired,
+  city: PropTypes.string.isRequired,
+};
+
+export default MainPageWrapper;
--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import PlaceCard from '../place-card/place-card';
 import Header from '../header/header';
 import offerProp from '../app/offer.prop';
-import Map from '../map/map';
-import {ClassesCardType, LOCATIONS} from '../../const';
+import {LOCATIONS} from '../../const';
 import LocationList from '../location-list';
 import {connect} from 'react-redux';
+import MainPageWrapper from '../main-page-wrapper/main-page-wrapper';
 import MainEmpty from '../main-empty/main-empty';
-import SortForm from '../sort-form/sort-form';
 
 function MainPage(props) {
   const {offers, city} = props;
 
-  if (!offers.length) {
-    return <MainEmpty locations={LOCATIONS} />;
-  }
-
   return (
     <div className="page page--gray page--main">
       <Header />
-
-      <main className="page__main page__main--index">
+      <main className={`${!offers.length && 'page__main--index-empty'} page__main page__main--index`}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -29,24 +22,9 @@ function MainPage(props) {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offers.length} places to stay in {city}</b>
-              <SortForm />
-              <div className="cities__places-list places__list tabs__content">
-                {offers.map((offer, id) => {
-                  const keyValue = `place-card-${id}`;
-                  return <PlaceCard key={keyValue} offer={offer} cardType={ClassesCardType.MAIN} />;
-                })}
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map">
-                <Map city={offers[0].city} offers={offers} />
-              </section>
-            </div>
-          </div>
+          {offers.length
+            ? <MainPageWrapper offers={offers} city={city} />
+            : <MainEmpty />}
         </div>
       </main>
     </div>
